5f3063b40a470c57d3a46e56ddc79348c40a9c55
Grey <sergejyurskyj@yahoo.com>
2020-05-09 18:16:43 -0700

    Commited Section 6 Coding Challenge #2 - Testing Classes

--- Section 3 Testing Functions/first_project.py
+++ Section 3 Testing Functions/first_project.py
@@ -1,4 +1,3 @@
-
 def avg(*list_numbers: float) -> float:
 	total = 0
 	for num in list_numbers:
@@ -7,6 +6,3 @@ def avg(*list_numbers: float) -> float:
 		else:
 			raise TypeError("Wrong input data. Please make sure that everything is a number. ")
 	return total / len(list_numbers)
-
-
-
